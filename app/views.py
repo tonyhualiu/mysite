cf9d27a55725c2fbff199fb74b9b5f70e94b2035
Tony Liu <tonyhualiu1912@gmail.com>
2015-01-15 22:08:44 -0800

    route index template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,41 @@
 from app import app
+import bottle
+from bottle import static_file
+
+basePath = './app/static/theme/'
+name = 'tony'
+bottle.TEMPLATE_PATH.insert(0, './app/templates/')
+static_file = bottle.static_file
+template = bottle.template
 
 @app.route('/hello')
 def hello():
     return 'Hello World'
+
+
+##### Static Routes #####
+@app.route('/<filename:re:.*\.html>')
+def html(filename):
+    return static_file(filename, root = basePath)
+@app.route('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root= basePath)
+
+@app.route('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root= basePath)
+
+@app.route('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root= basePath)
+
+@app.route('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+    return static_file(filename, root= basePath)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    print bottle.TEMPLATE_PATH
+    return template('index.tpl', name = name)
+
