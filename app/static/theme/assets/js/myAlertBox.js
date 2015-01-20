3af28bf38226e5a412d6adee072bd3468936381a
Tony Liu <tonyhualiu1912@gmail.com>
2015-01-20 14:30:48 -0800

    login-box listen-to-enter

--- app/static/theme/assets/js/myAlertBox.js
+++ app/static/theme/assets/js/myAlertBox.js
@@ -1,22 +1,6 @@
 // This is a customerized alert message box
 
 function CustomAlert(){
-		this.render = function(dialog){
-			var winW = window.innerWidth;
-			var winH = window.innerHeight;
-			var dialogoverlay = document.getElementById('dialogoverlay');
-			var dialogbox = document.getElementById('dialogbox');
-			dialogoverlay.style.display = "block";
-			dialogoverlay.style.height = winH+"px";
-			
-			dialogbox.style.left = (winW/2) - (550 * .5)+"px";
-			dialogbox.style.top = "200px";
-			$("#dialogbox").fadeIn(300)
-			document.getElementById('dialogboxfoot').innerHTML = '<button type="button" onclick="myAlertBox.ok()"'+
-																'class="btn btn-default btn-sm" >submit</button>'+
-																'<button type="button" class="btn btn-default btn-sm"'+
-															   'onclick="myAlertBox.cancel()" >cancel</button>';
-		}
 		this.ok = function(){
 			url = '/admin';
 			username = document.getElementById('username').value;
@@ -35,8 +19,26 @@ function CustomAlert(){
 					}
 				}
 			};
-
 			jQuery.ajax(url, settings);	
+		}
+		
+		this.render = function(dialog){
+			var winW = window.innerWidth;
+			var winH = window.innerHeight;
+			var dialogoverlay = document.getElementById('dialogoverlay');
+			var dialogbox = document.getElementById('dialogbox');
+			dialogoverlay.style.display = "block";
+			dialogoverlay.style.height = winH+"px";
+			
+			
+			
+			dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+			dialogbox.style.top = "200px";
+			$("#dialogbox").fadeIn(300)
+			document.getElementById('dialogboxfoot').innerHTML = '<button type="button" onclick="myAlertBox.ok()"'+
+																'class="btn btn-default btn-sm" >submit</button>'+
+																'<button type="button" class="btn btn-default btn-sm"'+
+															   'onclick="myAlertBox.cancel()" >cancel</button>';
 		}
 		this.cancel = function(){
 			document.getElementById('dialogbox').style.display = "none";
@@ -48,3 +50,12 @@ function CustomAlert(){
 		}
 }
 var myAlertBox = new CustomAlert();
+$(document).ready(function(){
+	$('#password').keypress(function(event){
+								if(event.which == 13){
+									myAlertBox.ok();
+								}
+							}
+						);
+});
+
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 from app import views
 
 if __name__ == '__main__':
-    app.run(host = 'localhost', port = 5000, debug = True)
+    app.run(host = '192.168.0.17', port = 5000, debug = True)
